dcf6d49476d2449ab9fc0ebd26b87f81bc3471dc
Edward Palmer <edward@myaffiliates.com>
2024-12-18 16:06:49 +1000

    agent testing

--- pkgs/newrelic-php-agent.nix
+++ pkgs/newrelic-php-agent.nix
@@ -13,7 +13,7 @@ stdenv.mkDerivation {
     owner = "newrelic";
     repo = "newrelic-php-agent";
     rev = "v${distVersion}";
-    hash = "sha256-";
+    hash = "";
   };
   nativeBuildInputs =
     (with pkgs; [
