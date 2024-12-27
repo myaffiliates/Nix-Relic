6d64cb7dfb297f3449ecf3a3a544a9430f11b080
Edward Palmer <edward@myaffiliates.com>
2024-12-27 10:20:29 +1000

    testing php agent

--- pkgs/newrelic-php-agent.nix
+++ pkgs/newrelic-php-agent.nix
@@ -19,19 +19,24 @@ stdenv.mkDerivation rec {
   pname = "newrelic-php-agent";
   inherit version;
 
-  src = fetchFromGitHub {
-    owner = "newrelic";
-    repo = "newrelic-php-agent";
-    rev = "v${version}";
-    sha256 = "GOtjX8Oa6gkD28sFVsoVjI537MpABIAInNHJGjsul7U=";
+  src = fetchzip {
+    url = "https://github.com/newrelic/newrelic-php-agent/archive/refs/tags/v${version}.tar.gz";
+    sha256 = "0xw6cr5jgi1ir13q6apvrivwmmpr5j8vbymp0x6ll0kcv6366hnn";
   };
+
+  # src = fetchFromGitHub {
+  #   owner = "newrelic";
+  #   repo = "newrelic-php-agent";
+  #   rev = "v${version}";
+  #   sha256 = "GOtjX8Oa6gkD28sFVsoVjI537MpABIAInNHJGjsul7U=";
+  # };
 
   # internalDeps = [
   #   php.extensions.pgsql
   # ];
 
   nativeBuildInputs = [ pkg-config pkgs.php82.unwrapped ];
-  buildInputs = [ pcre2 pkgs.git ];
+  buildInputs = [ pcre2 pkgs.git autoconf automake libtool ];
 
   installPhase = ''
      cp -r agent/.libs/newrelic.so $out/libs
