449820709ff7fbc5062a3a029a2b5eb5fd098b06
Edward Palmer <edward@myaffiliates.com>
2024-12-27 15:16:39 +1000

    testing php agent

--- pkgs/newrelic-php-agent.nix
+++ pkgs/newrelic-php-agent.nix
@@ -23,19 +23,11 @@ stdenv.mkDerivation rec {
   src = fetchzip {
     url = "https://github.com/newrelic/newrelic-php-agent/archive/refs/tags/v${version}.tar.gz";
     sha256 = "sha256-GOtjX8Oa6gkD28sFVsoVjI537MpABIAInNHJGjsul7U=";
-    #sha256 = "0xw6cr5jgi1ir13q6apvrivwmmpr5j8vbymp0x6ll0kcv6366hnn";
   };
 
-  # src = fetchFromGitHub {
-  #   owner = "newrelic";
-  #   repo = "newrelic-php-agent";
-  #   rev = "v${version}";
-  #   sha256 = "GOtjX8Oa6gkD28sFVsoVjI537MpABIAInNHJGjsul7U=";
-  # };
-
-  # internalDeps = [
-  #   php.extensions.pgsql
-  # ];
+  internalDeps = [
+    php.extensions.pdo
+  ];
 
   nativeBuildInputs = [ pkg-config pkgs.php82.unwrapped ];
   buildInputs = [ pkgs.pcre pkgs.protobufc pkgs.gnumake pkgs.autoconf pkgs.gcc pkgs.automake pkgs.libtool pkgs.git ];
@@ -47,4 +39,9 @@ stdenv.mkDerivation rec {
      cp -r newrelic-php-agent/bin $out/bin  
   '';
 
+  meta = {
+    description = "New Relic PHP Agent";
+    homepage = "https://github.com/newrelic/newrelic-php-agent";
+    maintainers = "Edward Palmer";
+  };
 }
