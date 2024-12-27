1fa41d9bac926c91eaf884f5de97eeb295ede555
Edward Palmer <edward@myaffiliates.com>
2024-12-27 10:32:26 +1000

    testing php agent

--- pkgs/newrelic-php-agent.nix
+++ pkgs/newrelic-php-agent.nix
@@ -22,7 +22,8 @@ stdenv.mkDerivation rec {
 
   src = fetchzip {
     url = "https://github.com/newrelic/newrelic-php-agent/archive/refs/tags/v${version}.tar.gz";
-    sha256 = "0xw6cr5jgi1ir13q6apvrivwmmpr5j8vbymp0x6ll0kcv6366hnn";
+    sha256 = "sha256-GOtjX8Oa6gkD28sFVsoVjI537MpABIAInNHJGjsul7U=";
+    #sha256 = "0xw6cr5jgi1ir13q6apvrivwmmpr5j8vbymp0x6ll0kcv6366hnn";
   };
 
   # src = fetchFromGitHub {
