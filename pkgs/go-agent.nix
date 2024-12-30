b4d1ef3cd8c47e774d498837388cba4d5e17cc1e
Edward Palmer <edward@myaffiliates.com>
2024-12-30 13:28:31 +1000

    php agent and daemon successfully builds

--- pkgs/go-agent.nix
+++ pkgs/go-agent.nix
@@ -4,9 +4,9 @@
     pname = "go-agent";
     version = "3.35.1";
     
-  fetch = {
-    type = "git";
-    url = "https://github.com/newrelic/go-agent";
+  sce = fetchFromGitHub {
+    owner = "newrelic";
+    repo = "go-agent";
     rev =  "v${version}";
     sha256 = "1zbp1cqhxp0sz3faymam6h1f91r1gl8dnnjx7qg8r06bd5fbzllb";
   };
