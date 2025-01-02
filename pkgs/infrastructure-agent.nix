9d1ce44a9954bead154fa12c8f0f5df773094f29
Edward Palmer <edward@myaffiliates.com>
2025-01-02 23:20:45 +1000

    php agent testing

--- pkgs/infrastructure-agent.nix
+++ pkgs/infrastructure-agent.nix
@@ -7,7 +7,43 @@
   fetchzip,
   pkg-config,
 }: 
+let 
 
+  nag-sce = fetchzip {
+    url = "https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/nri-nagios_linux_${nagVersion}_amd64.tar.gz";
+    stripRoot = false;
+    sha256 = "sha256-99VcpXZuTg4PP193o1WV9Jl1FFr+Pm7iJgy0ygHhak8=";
+  };
+
+  nginx-sce = fetchzip {
+    url = "https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/nri-nginx_linux_${nginxVersion}_amd64.tar.gz";
+    stripRoot = false;
+    sha256 = "sha256-y1sNjQf8MPTUHWlRO4szk1jm4/Q/lXIKM7+aI4LcMQ0=";
+  };
+
+  php-sce =  fetchzip {
+    url = "https://download.newrelic.com/php_agent/release/newrelic-php5-${phpVersion}-linux.tar.gz";
+    sha256 = "sha256-acTNfszCcX6RKF+XY2yb4S/dahuyHoEWa11//ua6MaY=";
+
+  };
+
+  flex-sce = fetchzip {
+    url = "https://github.com/newrelic/nri-flex/releases/download/v${flexVersion}/nri-flex_linux_${flexVersion}_amd64.tar.gz";
+    stripRoot = false;
+    sha256 = "sha256-GMB86hg6B3WB1C6x5JzdO7Uo0lf0iyBXNqqfE5sXP+Q=";
+  };
+
+  fb =  builtins.fetchurl {
+    url = "https://github.com/newrelic/newrelic-fluent-bit-output/releases/download/v${fbVersion}/out_newrelic-linux-amd64-${fbVersion}.so";
+    sha256 = "0chy0w7aajb5mhxa6k1nbsgd2670xvsxj96wvchachf751ibdwzs";
+  };
+
+  fbParsers = builtins.fetchurl {
+    url = "https://github.com/newrelic/fluent-bit-package/blob/main/parsers.conf";
+    sha256 = "1rgc61mwczn31rs33w8ha843z2ywdjqn9aiilxp5v8w4q4gqp4l3";
+  };
+
+in
 #stdenv.mkDerivation rec {
 buildGoModule rec {
   pname = "infrastructure-agent";
@@ -46,52 +82,17 @@ buildGoModule rec {
     "tools/"
   ];
 
-  nag-sce = fetchzip {
-    url = "https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/nri-nagios_linux_${nagVersion}_amd64.tar.gz";
-    stripRoot = false;
-    sha256 = "sha256-99VcpXZuTg4PP193o1WV9Jl1FFr+Pm7iJgy0ygHhak8=";
-  };
-
-  nginx-sce = fetchzip {
-    url = "https://download.newrelic.com/infrastructure_agent/binaries/linux/amd64/nri-nginx_linux_${nginxVersion}_amd64.tar.gz";
-    stripRoot = false;
-    sha256 = "sha256-y1sNjQf8MPTUHWlRO4szk1jm4/Q/lXIKM7+aI4LcMQ0=";
-  };
-
-  php-sce =  fetchzip {
-    url = "https://download.newrelic.com/php_agent/release/newrelic-php5-${phpVersion}-linux.tar.gz";
-    sha256 = "sha256-acTNfszCcX6RKF+XY2yb4S/dahuyHoEWa11//ua6MaY=";
-
-  };
-
-  flex-sce = fetchzip {
-    url = "https://github.com/newrelic/nri-flex/releases/download/v${flexVersion}/nri-flex_linux_${flexVersion}_amd64.tar.gz";
-    stripRoot = false;
-    sha256 = "sha256-GMB86hg6B3WB1C6x5JzdO7Uo0lf0iyBXNqqfE5sXP+Q=";
-  };
-
-  fb =  builtins.fetchurl {
-    url = "https://github.com/newrelic/newrelic-fluent-bit-output/releases/download/v${fbVersion}/out_newrelic-linux-amd64-${fbVersion}.so";
-    sha256 = "0chy0w7aajb5mhxa6k1nbsgd2670xvsxj96wvchachf751ibdwzs";
-  };
-
-  fbParsers = builtins.fetchurl {
-    url = "https://github.com/newrelic/fluent-bit-package/blob/main/parsers.conf";
-    sha256 = "1rgc61mwczn31rs33w8ha843z2ywdjqn9aiilxp5v8w4q4gqp4l3";
-  };
-
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ pkgs.curl pkgs.autoconf pkgs.automake pkgs.libtool pkgs.bash ];
+  buildInputs = [ pkgs.curl ];
 
   installPhase = ''
-    mkdir -p $out/bin
+    # mkdir -p $out/bin
     mkdir -p $out/etc/newrelic-infra/logging.d
     mkdir -p $out/etc/newrelic-infra/integrations.d
     mkdir -p $out/lib
     mkdir -p $out/var/db/newrelic-infra/newrelic-integrations/logging
     mkdir -p $out/var/db/newrelic-infra/newrelic-integrations/bin
 
-    cp -r ${src}/usr/bin/* $out/bin
+    # cp -r ${src}/usr/bin/* $out/bin
     cp -r ${nag-sce}/* $out/
     cp -r ${nginx-sce}/* $out/
     cp -r ${php-sce}/agent/x64/newrelic-20220829.so $out/lib/newrelic.so
