baf7c18f3afe1cc692f0c2a8a21fe0d67d2eb972
Edward Palmer <edward@myaffiliates.com>
2025-01-03 09:22:38 +1000

    php agent testing

--- pkgs/infrastructure-agent.nix
+++ pkgs/infrastructure-agent.nix
@@ -98,8 +98,11 @@ buildGoModule rec {
     cp -r ${php-sce}/agent/x64/newrelic-20220829.so $out/lib/newrelic.so
     cp -r ${php-sce}/daemon/newrelic-daemon.x64 $out/bin/daemon
     cp -r ${flex-sce}/nri-flex $out/var/db/newrelic-infra/newrelic-integrations/bin
-    ${pkgs.curl}/bin/curl -L --silent '${fb}' --output $out/var/db/newrelic-infra/newrelic-integrations/logging/out_newrelic.so
-    ${pkgs.curl}/bin/curl -L --silent '${fbParsers}' --output $out/var/db/newrelic-infra/newrelic-integrations/logging/parsers.conf
+    cp -r ${fb} $out/var/db/newrelic-infra/newrelic-integrations/logging/out_newrelic.so
+    cp -r ${fbParsers} $out/var/db/newrelic-infra/newrelic-integrations/logging/parsers.conf
+
+    #curl -L --silent '${fb}' --output $out/var/db/newrelic-infra/newrelic-integrations/logging/out_newrelic.so
+    #curl -L --silent '${fbParsers}' --output $out/var/db/newrelic-infra/newrelic-integrations/logging/parsers.conf
   '';
 
   doCheck = false;
