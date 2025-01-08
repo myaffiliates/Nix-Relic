6d77bba1e40cfc90281fb738a73f13d9b95485ba
Edward Palmer <edward@myaffiliates.com>
2025-01-08 22:33:55 +1000

    add nri-redis

--- pkgs/infrastructure-agent.nix
+++ pkgs/infrastructure-agent.nix
@@ -48,8 +48,7 @@ let
 
   redis-sce = fetchzip {
     url = "https://github.com/newrelic/nri-redis/releases/download/v${redisVersion}/nri-redis_linux_${redisVersion}_amd64.tar.gz";
-    stripRoot = false;
-    sha256 = lib.fakeHash;
+    sha256 = "sha256-RVJsqIAfDYBf5MtefWgbDrQA33kQad4TndTxz8Akoc8=";
   };
 
   fb =  builtins.fetchurl {
