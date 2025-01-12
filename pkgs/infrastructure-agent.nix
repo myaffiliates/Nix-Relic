99b269a00d2b59572485aa0c4151002bbc16d8ab
Edward Palmer <edward@myaffiliates.com>
2025-01-13 08:30:08 +1000

    update agent

--- pkgs/infrastructure-agent.nix
+++ pkgs/infrastructure-agent.nix
@@ -72,13 +72,14 @@ buildGoModule rec {
   env.CGO_ENABLED = "0";
   
   preBuild = ''
+    export GOPROXY="direct"
+
     substituteInPlace go.sum \
       --replace-quiet 'v3.27.0 h1:Z3XB49d8FKjRcGzCyViCO9itBxiLPSpwjY1HlMvgamQ=' 'v3.35.1 h1:N43qBNDILmnwLDCSfnE1yy6adyoVEU95nAOtdUgG4vA=' \
       --replace-quiet 'v3.27.0/go.mod h1:TUzePinDc0BMH4Sui66rl4SBe6yOKJ5X/bRJekwuAtM=' 'v3.35.1/go.mod h1:GNTda53CohAhkgsc7/gqSsJhDZjj8vaky5u+vKz7wqM='
 
     substituteInPlace go.mod \
       --replace-quiet 'go-agent/v3 v3.27.0' 'go-agent/v3 v3.35.1'
-
     go mod vendor
   '';
 
