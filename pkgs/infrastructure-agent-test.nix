4b2786fa2c434f9992ae69df68cd0cf73a17b29e
Edward Palmer <edward@myaffiliates.com>
2025-01-15 16:35:33 +1000

    go.sum add

--- pkgs/infrastructure-agent-test.nix
+++ pkgs/infrastructure-agent-test.nix
@@ -56,8 +56,8 @@ let
 
 
 in
-  # stdenv.mkDerivation rec {
-buildGoModule rec {
+stdenv.mkDerivation rec {
+  # buildGoModule rec {
     pname = "infrastructure-agent";
     inherit version;
   
@@ -66,23 +66,10 @@ buildGoModule rec {
     repo = "infrastructure-agent";
     rev = version;
     hash = "sha256-IfPiexh6vPOFkMz1OqNouozKJoKXeQMYYhaPg/tU0sg=";
-  };
-
-  vendorHash = "sha256-1acOfcjIvFG9pbkSktFQ4AypymvSphHZ5gvkvmkIkU8=";
-  #vendorHash = "sha256-Mi6X5sENfKBjCbG/M0WxpCYTPhF6xAJ2WyXs8S7SIv8=";
-
-  nativeBuildInputs = [ pkg-config ];
-  buildInputs = [ pkgs.pcre pkgs.protobufc pkgs.cmake pkgs.gnumake pkgs.autoconf pkgs.gcc pkgs.automake pkgs.libtool pkgs.git pkgs.bash pkgs.go ];
-
-  env.NIX_CFLAGS_COMPILE = "-O2";
-  env_GO_BIN_PATH = "${pkgs.go}/bin/go";
-
-  proxyVendor = true;
-
-  preBuild = ''
-    export HOME=$(pwd)
-    export GOPROXY="direct"
-    export PATH="${pkgs.git}/bin:$PATH"
+    postFetch = ''
+      export HOME=$(pwd)
+      export GOPROXY="direct"
+      export PATH="${pkgs.git}/bin:${pkgs.go}/bin:$PATH"
 
     substituteInPlace go.sum \
       --replace-quiet 'v3.27.0 h1:Z3XB49d8FKjRcGzCyViCO9itBxiLPSpwjY1HlMvgamQ=' 'v3.35.1 h1:N43qBNDILmnwLDCSfnE1yy6adyoVEU95nAOtdUgG4vA=' \
@@ -95,9 +82,18 @@ buildGoModule rec {
       --replace-quiet 'include $(INCLUDE_TOOLS' '# include $(INCLUDE_TOOLS' \
       --replace-quiet 'include $(INCLUDE_TEST' '# include $(INCLUDE_TEST'
     
-    ${pkgs.go}/bin/go mod tidy
+    go mod tidy
+    '';    
+  };
 
-  '';
+  # vendorHash = "sha256-1acOfcjIvFG9pbkSktFQ4AypymvSphHZ5gvkvmkIkU8=";
+  #vendorHash = "sha256-Mi6X5sENfKBjCbG/M0WxpCYTPhF6xAJ2WyXs8S7SIv8=";
+
+  nativeBuildInputs = [ pkg-config ];
+  buildInputs = [ pkgs.pcre pkgs.protobufc pkgs.cmake pkgs.gnumake pkgs.autoconf pkgs.gcc pkgs.automake pkgs.libtool pkgs.git pkgs.bash pkgs.go ];
+
+  env.NIX_CFLAGS_COMPILE = "-O2";
+  env_GO_BIN_PATH = "${pkgs.go}/bin/go";
 
   modPostBuild = ''
     mkdir -p $out/bin
