b0216cf9d76ed574e16a2fe0187682365c2b0ba6
Edward Palmer <edward@myaffiliates.com>
2025-01-16 11:30:29 +1000

    go.sum add

--- pkgs/go-agent.nix
+++ pkgs/go-agent.nix
@@ -48,11 +48,11 @@ in
 
   buildInputs = [ stdenv pkgs.go pkgs.git ];
 
-  subPackages = [
-    "newrelic"
-    "integrations/logcontext-v2/logWriter"
-    "integrations/logcontext-v2/nrmysql"
-  ];
+  # subPackages = [
+  #   "newrelic"
+  #   "integrations/logcontext-v2/logWriter"
+  #   "integrations/logcontext-v2/nrmysql"
+  # ];
 
   env.CGO_ENABLED = if stdenv.hostPlatform.isDarwin then "1" else "0";
   env.HOME = "$(pwd)";
