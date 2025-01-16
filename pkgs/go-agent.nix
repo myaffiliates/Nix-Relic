d127835d6673c13c1b0d69e2d89ca5e534d5bb0b
Edward Palmer <edward@myaffiliates.com>
2025-01-16 10:14:52 +1000

    go.sum add

--- pkgs/go-agent.nix
+++ pkgs/go-agent.nix
@@ -37,7 +37,7 @@ in
       export HOME=$(pwd)
       cd $out/v3
       go mod tidy
-      go mod download 
+      go mod download github.com/newrelic/go-agent/v3 
       #github.com/newrelic/go-agent/v3/integrations/logcontext-v2/logWriter
       #go mod download github.com/newrelic/go-agent/v3/integrations/logcontext-v2/nrwriter      
       # cd $out/v3/integrations/logcontext-v2/logWriter
