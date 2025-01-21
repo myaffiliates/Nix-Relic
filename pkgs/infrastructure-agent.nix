ce51dfa9dbbf23139f885f0cc82e190c68e416b0
Edward Palmer <edward@myaffiliates.com>
2025-01-21 15:18:16 +1000

    agent updates

--- pkgs/infrastructure-agent.nix
+++ pkgs/infrastructure-agent.nix
@@ -57,7 +57,9 @@ buildGoModule rec {
     postFetch = ''
       export PATH="${pkgs.git}/bin:${pkgs.go}/bin:$PATH"
       cd $out
-      go mod edit github.com/newrelic/go-agent/v3 v3.36.0
+      go mod edit -replace (
+        github.com/newrelic/go-agent/v3 => github.com/newrelic/go-agent/v3 v3.36.0
+      )
       go mod tidy
       go mod vendor
     '';  
