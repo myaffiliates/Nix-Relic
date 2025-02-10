6b96c263db71957805540ea30609e8b86b69d537
Edward Palmer <edward@myaffiliates.com>
2025-02-10 13:47:30 +1000

    different fluentbit build

--- pkgs/fluent-bit-output.nix
+++ pkgs/fluent-bit-output.nix
@@ -10,8 +10,6 @@
 let
   version = "2.1.0";
  
-  system = "x86_64-linux";
-
   oldGo120 = import (builtins.fetchTarball {
       url = "https://github.com/NixOS/nixpkgs/archive/0a25e2c87e784bf7e06e7833ec0e06d34836959a.tar.gz";
       sha256 = "sha256:0gp3rincm83m2b26dbyvr5dh4zk5i0cbva3lzbzd38k1c6mfyyws";
@@ -19,12 +17,10 @@ let
 
   go-version = oldGo120.go;
 
-  
-
 in
   buildGoModule rec {
     pname = "fluent-bit-output";
-    inherit version go-version pkgs system;
+    inherit version go-version pkgs;
 
     src = fetchzip {
       url = "https://github.com/newrelic/newrelic-fluent-bit-output/archive/refs/tags/v${version}.tar.gz";
@@ -39,18 +35,18 @@ in
   ];
 
   buildInputs = [ stdenv pkgs.pcre pkgs.protobufc pkgs.cmake pkgs.gnumake pkgs.autoconf pkgs.gcc go-version ];
-  # env.HOME = "$(pwd)";
-  # env.CGO_ENABLED = if stdenv.hostPlatform.isDarwin then "1" else "0";
+  env.HOME = "$(pwd)";
+  env.CGO_ENABLED = if stdenv.hostPlatform.isDarwin then "1" else "0";
   # env.GOOS = "linux";
   # env.GOARCH = "amd64";
 
-  buildPhase = ''
-    export GOPROXY="direct"
-    export PATH="${pkgs.git}/bin:${go-version}/bin:$PATH"
-    export HOME=$(pwd)
-    cd $src
-    make linux/amd64
-  '';
+  # buildPhase = ''
+  #   export GOPROXY="direct"
+  #   export PATH="${pkgs.git}/bin:${go-version}/bin:$PATH"
+  #   export HOME=$(pwd)
+  #   cd $src
+  #   make linux/amd64
+  # '';
 
   doCheck = false;
 
