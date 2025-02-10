b1f54abac2d885921aeac720df58e31b98a3bd8a
Edward Palmer <edward@myaffiliates.com>
2025-02-10 11:23:53 +1000

    different fluentbit build

--- pkgs/fluent-bit-output.nix
+++ pkgs/fluent-bit-output.nix
@@ -17,7 +17,7 @@ in
 
     src = fetchzip {
       url = "https://github.com/newrelic/newrelic-fluent-bit-output/archive/refs/tags/v${version}.tar.gz";
-      sha256 = lib.fakeSha256;
+      sha256 = "sha256-01SyjlHlt/yd1r3QU0J2siMHIX+30xoMeYZrDi8iQ90=";
     };
 
   vendorHash = lib.fakeHash;
