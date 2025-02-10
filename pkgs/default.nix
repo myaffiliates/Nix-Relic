ecf0f633173984763c5fe1c20dd1916a6eee3d18
Edward Palmer <edward@myaffiliates.com>
2025-02-10 11:14:31 +1000

    different fluentbit build

--- pkgs/default.nix
+++ pkgs/default.nix
@@ -1,12 +1,13 @@
 # You can build the below packages using 'nix build .#example'
 {pkgs}: rec {
-  infrastructure-agent = pkgs.callPackage ./infrastructure-agent.nix {};
-  test-agent = pkgs.callPackage ./infrastructure-agent-test.nix {};  
-  ocb = pkgs.callPackage ./ocb.nix {};
+  fluent-bit-output = pkgs.callPackage ./fluent-bit-output.nix {};
   go-agent = pkgs.callPackage ./go-agent.nix {};
+  infrastructure-agent = pkgs.callPackage ./infrastructure-agent.nix {};
+  newrelic-cli = pkgs.callPackage ./newrelic-cli.nix {};
   newrelic-php-agent = pkgs.callPackage ./newrelic-php-agent.nix {};
   newrelic-php-daemon = pkgs.callPackage ./newrelic-php-daemon.nix {};
-  newrelic-cli = pkgs.callPackage ./newrelic-cli.nix {};
+  ocb = pkgs.callPackage ./ocb.nix {};
+  test-agent = pkgs.callPackage ./infrastructure-agent-test.nix {}; 
 
   nr-otel-collector = pkgs.callPackage ./nr-otel-collector {
     # nr-otel-collector needs a specific version of ocb at this moment, hence this hack
---
+++ pkgs/fluent-bit-output.nix
@@ -0,0 +1,50 @@
+{ 
+  lib,
+  stdenv,
+  buildGoModule,
+  fetchFromGitHub,
+  fetchzip,
+  ... 
+}:
+let
+  version = "2.1.0";
+
+in
+  buildGoModule rec {
+    pname = "fluent-bit-output";
+    inherit version;
+
+    src = fetchzip {
+      url = "https://github.com/newrelic/newrelic-fluent-bit-output/archive/refs/tags/v${version}.tar.gz";
+      sha256 = lib.fakeSha256;
+    };
+
+  vendorHash = lib.fakeHash;
+
+  ldflags = [
+    "-w"
+    "-s"
+    "-X main.version=${version}"
+  ];
+
+  buildInputs = [ stdenv pkgs.pcre pkgs.protobufc pkgs.cmake pkgs.gnumake pkgs.autoconf pkgs.gcc pkgs.go ];
+  env.HOME = "$(pwd)";
+  env.CGO_ENABLED = if stdenv.hostPlatform.isDarwin then "1" else "0";
+
+  doCheck = false;
+
+  postInstall = ''
+    mkdir -p $out
+    cp -r ${build}/* $out/
+  '';
+
+  meta = {
+    description = "New Relic PHP Agent Daemon";
+    mainProgram = "fluent-bit-output";
+  };
+}
+
+
+
+
+
