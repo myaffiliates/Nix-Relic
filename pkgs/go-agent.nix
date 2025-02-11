e50ffd495c347eae4c8dd9272d1828c3d4114cdc
Edward Palmer <edward@myaffiliates.com>
2025-02-11 10:48:33 +1000

    different fluentbit build

--- pkgs/go-agent.nix
+++ pkgs/go-agent.nix
@@ -1,5 +1,76 @@
 { pkgs, lib, stdenv, buildGoModule, fetchFromGitHub }:
-# let
+let
+
+maingo = pkgs.writeTextFile {
+  name = "main.go";
+  text = ''  
+package main
+
+import (
+	"context"
+	"log"
+	"os"
+	"time"
+
+	"github.com/newrelic/go-agent/v3/integrations/logcontext-v2/nrlogrus"
+	newrelic "github.com/newrelic/go-agent/v3/newrelic"
+	"github.com/sirupsen/logrus"
+)
+
+func doFunction2(txn *newrelic.Transaction, e *logrus.Entry) {
+	defer txn.StartSegment("doFunction2").End()
+	e.Error("In doFunction2")
+}
+
+func doFunction1(txn *newrelic.Transaction, e *logrus.Entry) {
+	defer txn.StartSegment("doFunction1").End()
+	e.Trace("In doFunction1")
+	doFunction2(txn, e)
+}
+
+func main() {
+	app, err := newrelic.NewApplication(
+		newrelic.ConfigAppName("Vagrant ZBan"),
+		newrelic.ConfigLicense(os.Getenv("NEW_RELIC_LICENSE_KEY")),
+		newrelic.ConfigInfoLogger(os.Stdout),
+		newrelic.ConfigAppLogForwardingEnabled(true),
+
+		// If you wanted to forward your logs using a log forwarder instead
+		// newrelic.ConfigAppLogDecoratingEnabled(true),
+		// newrelic.ConfigAppLogForwardingEnabled(false),
+	)
+	if nil != err {
+		log.Panic("Failed to create application", err)
+	}
+
+	log := logrus.New()
+	log.SetLevel(logrus.TraceLevel)
+	// Enable New Relic log decoration
+	log.SetFormatter(nrlogrus.NewFormatter(app, &logrus.TextFormatter{}))
+	log.Trace("waiting for connection to New Relic...")
+
+	err = app.WaitForConnection(10 * time.Second)
+	if nil != err {
+		log.Panic("Failed to connect application", err)
+	}
+	defer app.Shutdown(10 * time.Second)
+	log.Info("application connected to New Relic")
+	log.Debug("Starting transaction now")
+	txn := app.StartTransaction("main")
+
+	// Add the transaction context to the logger. Only once this happens will
+	// the logs be properly decorated with all required fields.
+	e := log.WithContext(newrelic.NewContext(context.Background(), txn))
+
+	doFunction1(txn, e)
+
+	e.Info("Ending transaction")
+	txn.End()
+}
+  '';
+};
+
+
 # sum = pkgs.writeTextFile {
 #   name = "go.sum";
 #   text = ''                                                                         
@@ -20,12 +91,12 @@
 #   '';
 # };
 
-# in
+in
   buildGoModule rec {
 # stdenv.mkDerivation rec {
     pname = "go-agent";
     version = "3.36.0";
-    # inherit sum;
+    inherit maingo;
 
   src = fetchFromGitHub {
     owner = "newrelic";
@@ -40,6 +111,7 @@
       go mod tidy
       go mod vendor
       cd $out/v3/integrations/logcontext-v2/nrwriter
+      cp ${maingo} main.go
       go mod tidy
       go mod vendor
     '';
